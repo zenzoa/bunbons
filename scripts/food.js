3e6c25759b257a808c70516e7fce61ac2393b330
SG <sarah.gould@gmail.com>
2020-12-27 21:14:26 -0800

    draw food graphics

--- scripts/food.js
+++ scripts/food.js
@@ -1,9 +1,25 @@
+let foodSprites = {
+    'mushrooms': 180,
+    'cloud-dumplings': 181,
+    'juice-orb': 182
+}
+
 class Food extends GameObject {
     constructor() {
-        super(10, 10)
+        super(24, 24)
 
-        this.name = 'food'
+        // temp
+        let foodType = random(Object.keys(foodSprites))
+        let foodSpriteIndex = foodSprites[foodType]
+
+        this.name = foodType
         this.pos = randomPoint()
+
+        this.offsetX = -4
+        this.offsetY = -8
+
+        this.uneatenSpriteIndex = foodSpriteIndex
+        this.eatenSpriteIndex = foodSpriteIndex + 20
 
         this.isRefilling = false
         this.refillLength = floor(random(600, 2100))
@@ -32,17 +48,16 @@ class Food extends GameObject {
 
         // find upper-left corner of sprite
         let x = floor(this.pos.x - (this.width / 2) + this.offsetX)
-        let y = floor(this.pos.y - this.height + this.offsetY - this.jumpY)
-        translate(x, y)
+        let y = floor(this.pos.y - this.height + this.offsetY)
+
+        let spriteIndex = this.isRefilling ? this.eatenSpriteIndex : this.uneatenSpriteIndex
+        image(baseSpritesheet.get(spriteIndex), x, y)
 
         // draw debug lines
         if (DEBUG) {
             noFill()
-            if (this.isRefilling) stroke('pink')
-            else stroke('lightblue')
-            rect(0, 0, this.width, this.height)
+            stroke('lightblue')
+            rect(x - this.offsetX, y - this.offsetY, this.width, this.height)
         }
-
-        pop()
     }
 }
