06748a845ab2d16f9ddf3471ca60cb17d5d1c052
Sarah Gould <sarah.gould@gmail.com>
2022-07-06 17:54:20 -0700

    sneak in dna for mousepunk and dragon

--- scripts/egg.js
+++ scripts/egg.js
@@ -146,6 +146,46 @@ class Egg extends Toy {
                 tailUsesPrimaryColor: false
             })
 
+        } else if (bunbonDNA === 'mousepunk') {
+            this.bunbonDNA = Bunbon.randomDNA({
+                color: 'grey',
+                secondaryColor: 'pink',
+                ears: 'mouse',
+                tail: 'rat',
+                back: 'none',
+                head: 'mohawk',
+                pattern: 'none',
+                earsUsePrimaryColor: true,
+                tailUsesPrimaryColor: true,
+                maxSpeed: 0.7,
+                jumpChance: 0.05,
+                ageToAdulthood: 120,
+                hungerRate: 70,
+                boredomRate: 80,
+                lonelinessRate: 70,
+                sleepinessRate: 30
+            })
+
+        } else if (bunbonDNA === 'dragon') {
+            this.bunbonDNA = Bunbon.randomDNA({
+                color: 'green',
+                secondaryColor: 'purple',
+                ears: 'dragon',
+                tail: 'slug',
+                back: 'batwings',
+                head: 'spikes',
+                pattern: 'none',
+                earsUsePrimaryColor: true,
+                tailUsesPrimaryColor: true,
+                maxSpeed: 0.3,
+                jumpChance: 0.08,
+                ageToAdulthood: 480,
+                hungerRate: 50,
+                boredomRate: 80,
+                lonelinessRate: 20,
+                sleepinessRate: 80
+            })
+
         } else {
             this.bunbonDNA = bunbonDNA || Bunbon.randomDNA()
         }
